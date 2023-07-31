d239b9a44fa7b55cc451ae1a033f31900bd6f161
Julien Moutoussamy <jmoutous@z4r4p7.42lyon.fr>
2023-07-31 15:25:04 +0200

    ADD: move.c

---
+++ sources/utils/move.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/31 14:35:35 by jmoutous          #+#    #+#             */
+/*   Updated: 2023/07/31 14:39:52 by jmoutous         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cube3d.h"
+
+int	ft_key(int key, t_data *game)
+{
+	if (key == KEY_ESC)
+		ft_quit(game);
+	// if (ft_check_position(key, game) == 0 && game->result != 'W')
+	// {
+	// 	if (key == 119 || key == 65362)
+	// 		ft_move(game, &game->player_y, -1, 'L');
+	// 	if (key == 115 || key == 65364)
+	// 		ft_move(game, &game->player_y, 1, 'R');
+	// 	if (key == 97 || key == 65361)
+	// 		ft_move(game, &game->player_x, -1, 'L');
+	// 	if (key == 100 || key == 65363)
+	// 		ft_move(game, &game->player_x, 1, 'R');
+	// }
+	return (0);
+}
