80ba4bfe517fc3a941f1ed23455f545c55c94dac
Julien Moutoussamy <jmoutous@z4r7p7.42lyon.fr>
2023-08-03 17:53:11 +0200

    FIX: bug: can't move when touching east or south wall

--- sources/utils/player_collision.c
+++ sources/utils/player_collision.c
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 15:42:00 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/08/03 17:46:04 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/08/03 17:52:08 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,10 @@ int	ft_check_collision(t_data *game, int new_x, int new_y)
 	if (new_x - player_radius < 0 || new_x + player_radius > 1280
 		|| new_y - player_radius < 0 || new_y + player_radius > 720)
 		return (1);
-	new_y_w = (new_y + player_radius) / 80;
+	new_y_w = (new_y + player_radius - 1) / 80;
 	new_y_e = (new_y - player_radius) / 80;
 	new_x_n = (new_x - player_radius) / 80;
-	new_x_s = (new_x + player_radius) / 80;
+	new_x_s = (new_x + player_radius - 1) / 80;
 	if (game->map[new_y_w][new_x_n] == '1'
 		|| game->map[new_y_e][new_x_n] == '1'
 		|| game->map[new_y_w][new_x_s] == '1'
