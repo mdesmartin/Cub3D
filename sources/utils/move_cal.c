fed7972bfac68b571c131133b687984df70fc78d
Julien Moutoussamy <jmoutous@z4r6p7.42lyon.fr>
2023-08-05 18:21:31 +0200

    FIX: strafe function correctly

--- sources/utils/move_cal.c
+++ sources/utils/move_cal.c
@@ -6,13 +6,13 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 15:57:08 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/08/05 16:50:02 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/08/05 18:20:38 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cube3d.h"
 
-static void	ft_calculate_line(t_line *new_pos, int forward)
+static void	ft_calculate_line(t_line *new_pos, int direction)
 {
 	float	e2;
 	int		i;
@@ -25,12 +25,12 @@ static void	ft_calculate_line(t_line *new_pos, int forward)
 		if (e2 >= new_pos->dy)
 		{
 			new_pos->e += new_pos->dy;
-			new_pos->x0 += new_pos->sx * forward;
+			new_pos->x0 += new_pos->sx * direction;
 		}
 		if (e2 <= new_pos->dx)
 		{
 			new_pos->e += new_pos->dx;
-			new_pos->y0 += new_pos->sy * forward;
+			new_pos->y0 += new_pos->sy * direction;
 		}
 		i++;
 	}
@@ -40,10 +40,20 @@ void	ft_position_cal(t_data *game, int forward, int strafe)
 {
 	t_line	new_pos;
 
-	(void) strafe;
-	ft_add_x_line(&new_pos, game->x_player, WIN_WIDTH / 2, game->degree);
-	ft_add_y_line(&new_pos, game->y_player, WIN_HEIGTH / 2, game->degree);
-	ft_calculate_line(&new_pos, forward);
+	if (forward != 0)
+	{
+		ft_add_x_line(&new_pos, game->x_player, WIN_WIDTH / 2, game->degree);
+		ft_add_y_line(&new_pos, game->y_player, WIN_HEIGTH / 2, game->degree);
+		ft_calculate_line(&new_pos, forward);
+	}
+	else if (strafe != 0)
+	{
+		ft_add_x_line(&new_pos, game->x_player, WIN_WIDTH / 2,
+			game->degree - M_PI_2);
+		ft_add_y_line(&new_pos, game->y_player, WIN_HEIGTH / 2,
+			game->degree - M_PI_2);
+		ft_calculate_line(&new_pos, strafe);
+	}
 	if (ft_check_collision(game, new_pos.x0, game->y_player) == 0)
 		game->x_player = new_pos.x0;
 	if (ft_check_collision(game, game->x_player, new_pos.y0) == 0)
